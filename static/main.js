1bda7f4027484fecdd09ea23e3cfee117b7728a5
alina <agutierrez72@ucmerced.edu>
2020-01-30 09:43:59 -0800

    changed alert to output to webpage

--- static/main.js
+++ static/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $("#button").click(function(){
         var name = $("#textbox").val();
-
+        
         var msg = "Hello " + name;
-        alert(msg);
+        document.getElementById("demo").innerHTML = msg;
+        //alert(msg);
     })
 });
