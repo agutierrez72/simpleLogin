a3b62002774d208420db88613fcc0e21a54c2029
alina <agutierrez72@ucmerced.edu>
2020-01-30 11:52:05 -0800

    check valid chars of name & hit button using enter

--- static/main.js
+++ static/main.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
+    //text field will hit button on hit of enter button
+    $("#textbox").keyup(function(event){
+        if(event.keyCode === 13){
+            $("#button").click();
+        }
+    })
     $("#button").click(function(){
         var name = $("#textbox").val();
+
+        //check name has no symbols
+        if (name.match(/^[a-zA-Z_\s]+$/)) {
+            // Success
+            var msg = "Hello " + name;
+            document.getElementById("demo").innerHTML = msg;
+        }
+        else{
+            alert("Invalid Entry");
+        }
         
-        var msg = "Hello " + name;
-        document.getElementById("demo").innerHTML = msg;
         //alert(msg);
     })
 });
